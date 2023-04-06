d59245a18a70988f2645cdd0f10332ee4953d017
Pathara Norasethasopon <pathara.no@bdms.co.th>
2023-04-06 18:03:20 +0700

    Remote to content 1 and 2

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+import Area1 from "content1/Area1";
+import Area2 from "content2/Area2";
+
+const App = () => (
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <div>Name: layout</div>
+    <div>Framework: react</div>
+    <div>Language: TypeScript</div>
+    <div>CSS: Tailwind</div>
+
+    <div class="grid grid-cols-3 gap-4">
+      <div className="">
+        <Area1 />
+      </div>
+      <div className="col-span-2">
+        <Area2 />
+      </div>
+    </div>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
