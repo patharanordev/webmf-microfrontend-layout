d88372bb1edf620013eb49960d18f114c3f7475a
Pathara Norasethasopon <pathara.no@bdms.co.th>
2023-05-12 22:12:46 +0700

    fix: ReferenceError: React is not defined

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const ExampleInAppNotification = (props: any) => {
   const [notificationPermission, setNotificationPermission] = useState(Notification.permission);
