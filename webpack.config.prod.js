c1ee289e8afdf89e4ad3076858b418b43f5e719f
Pathara Norasethasopon <pathara.no@bdms.co.th>
2023-05-13 15:16:17 +0700

    fix: unused env

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,17 +1,8 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const webpack = require('webpack');
-const dotenv = require('dotenv');
 
 const deps = require("./package.json").dependencies;
-// call dotenv and it will return an Object with a parsed key 
-const env = dotenv.config().parsed;
-  
-// reduce it to a nice object, the same as before
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
 
 module.exports = (_, argv) => ({
   output: {
@@ -77,6 +68,5 @@ module.exports = (_, argv) => ({
     new webpack.ProvidePlugin({
       process: 'process/browser',
     }),
-    new webpack.DefinePlugin(envKeys)
   ],
 });
